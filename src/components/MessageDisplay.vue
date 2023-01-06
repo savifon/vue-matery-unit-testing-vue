c3459ff033b09947e80c135e798603ea8b904fea
savio.fonseca <savio.fonseca@ish.com.br>
2023-01-06 13:29:11 -0300

    Testing API Calls

---
+++ src/components/MessageDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p v-if="error" data-testid="message-error">{{ error }}</p>
+    <p v-else-if="message.text" data-testid="message">{{ message.text }}</p>
+  </div>
+</template>
+
+<script>
+import { getMessage } from "@/services/axios.js";
+
+export default {
+  data() {
+    return {
+      message: {
+        text: "",
+      },
+      error: null,
+    };
+  },
+
+  async created() {
+    try {
+      this.message = await getMessage();
+    } catch (err) {
+      this.error = "Oops! Something went wrong.";
+    }
+  },
+};
+</script>
